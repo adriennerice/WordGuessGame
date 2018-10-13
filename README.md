d52cf02e684c0a4f4a0e2d51673a4b3731e44783
Adrienne Rice <adrienne.rice@gmail.com>
2018-10-12 23:33:08 -0400

    completed homework w/ 2 minor issues

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WordGuessGame
-Word Guess Game similar to Hangman
+Ocean Hangman is a Word Guess Game similar to Hangman.
+
+Rules: Guess the correct letters to spell a word. Select a letter to start.
+
+Issue: Accepts non letters as input. Need to add a check for non letters and require the player to enter a letter.
+
+Issue: Used indexOf() method to check if letter was already guessed. Will not find the same letter multiple times if it is in the word multiple times. Only used words without reoccuring letters.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,63 +3,59 @@
 var seaCreatures = ["whale", "squid", "shark", "sponge"];
 var lettersGuessed = [];
 var lettersWrong = [];
+var winMessage = "You win! Let's play again!";
+var looseMessage = "You loose. Out of guesses. Please try again.";
+var blankMessage = "";
+var startMessage = "Rules: Guess the correct letters to spell a word. Select a letter to start."
 
 var userGuess;
-//var guessTotal = 0;  //++
 
 var wins = 0;
+var loss = 0;
 var lives = 5;
 
 
 // Randomly picks sea creature
 var answer = ""; 
-
+document.getElementById("message").innerHTML = startMessage;
   
 // [FUNCTIONS]
 
 //Start Function
 
-function start()
-{
+function start(){
   lettersGuessed = [];
   lettersWrong = [];
   lives = 5;
+  
+  document.getElementById("guessRemain").innerHTML = lives;
+  document.getElementById("wins").innerHTML = wins;
+  document.getElementById("loss").innerHTML = loss;
 
   answer = seaCreatures[Math.floor(Math.random() * seaCreatures.length)];
   
-  for(var i = 0; i < answer.length; i++)
-    {
+  for(var i = 0; i < answer.length; i++){
     lettersGuessed[i] = "_";
-    //console.log(lettersGuessed[i]);
     }
-    // dont try to save a joined array into another array
-
-    // underScore = lettersGuessed.join(''); //need to space these out with CSS
-    // console.log(underScore);
-    // console.log(lettersGuessed);
+   
   document.getElementById("word").innerHTML = lettersGuessed.join('');
-
 };
 
 
-//Check letters in word and new game function
+//Check letters in word and start new game Function
+
 function check(){
   document.onkeyup = function(event) {
+    document.getElementById("message").innerHTML = blankMessage;
 
      // Save user key press as lowercase letter
      userGuess = event.key.toLowerCase(); //checks if a string? 
-     
-    // ***Check if only a string was entered
     
-
      // [Check if letter guessed is already guessed]
      // Guess letter and show if in word
-     for(var i = 0; i < answer.length; i++)
-     {
-      if(lettersGuessed.indexOf(userGuess) < 0) // ***issue if word has multiple of same letter
-      {
-        if(userGuess === answer[i])
-        {
+     for(var i = 0; i < answer.length; i++){
+      if(lettersGuessed.indexOf(userGuess) < 0){ // ***issue if word has multiple of same letter
+        if(userGuess === answer[i]){
           lettersGuessed[i] = userGuess;
           document.getElementById("word").innerHTML = lettersGuessed.join('');
         }
@@ -67,34 +63,29 @@ function check(){
     }
      // [Check if wrong letter is already guessed]
      // List out wrong letters guessed
-     if (lettersGuessed.indexOf(userGuess) < 0 && lettersWrong.indexOf(userGuess) < 0) 
-     {
+     if (lettersGuessed.indexOf(userGuess) < 0 && lettersWrong.indexOf(userGuess) < 0){
       lettersWrong.push(userGuess);
       lives--;
-      document.getElementById("wrong").innerHTML = lettersWrong.join(' '); 
+      document.getElementById("guessRemain").innerHTML = lives;
+      document.getElementById("guessed").innerHTML = lettersWrong.join(' '); 
+      
       if (lives === 0){
-        console.log("You loose. Out of guesses");
+        document.getElementById("message").innerHTML = "The word is " + answer + ". " + looseMessage;
+        console.log(looseMessage);
+        loss++;
+        document.getElementById("loss").innerHTML = loss;
         start();
       } 
      }
-     
-     // [Where to place this code. Guess counter and exit game]
-    //  guessTotal++;
-
-    //  console.log(guessTotal);
-    //  // Check if out of guess
-    //  if (guessTotal === lives){
-    //   console.log("You loose. Out of guesses");
-    //   start();
-    //  }
 
       //Check of word was guessed
       if (lettersGuessed.join('') === answer)
       {
-        console.log("You win");
-        //You guessed the correct word "word"
-        //play music
-        wins ++;  //output wins
+        document.getElementById("message").innerHTML = "The word is " + answer + ". " + winMessage;
+        console.log(winMessage);
+         
+        wins++;  //output wins
+        document.getElementById("wins").innerHTML = wins;
         start();
       }
      }  
